7c448db82431247848e5fe32d250d335995b4972
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-12-12 12:56:38 +0600

    feat: test server with http://localhost:5500/api/v1/auth/test with 200 message: Auth route is working fine

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,4 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
+// Define test route
+router.get("/test", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Auth route is working fine",
+  });
+});
+
 export const AuthRoutes = router;
