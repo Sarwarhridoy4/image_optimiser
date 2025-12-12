909d6fe072c5ca5e3cdd53a0eded3a6cf80a2643
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-12-12 12:15:11 +0600

    chord:added and connecyted with mongodb atlas

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,12 +1,12 @@
-
 // src/config/db.ts
 import mongoose from "mongoose";
 import envConfig from "./env.js";
 
+const URI = envConfig.DB_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(envConfig.DB_URL);
+    await mongoose.connect(URI);
     console.log(`🟢 MongoDB connected at ${envConfig.NODE_ENV}`);
   } catch (error) {
     console.error("🔴 MongoDB connection failed:", error);
